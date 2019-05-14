b2c90750a01fe530c544dfae8ae3cca9cd91844d
keeratigibb <keerati.gib@gmail.com>
2019-05-14 17:10:15 +0700

    Update service uuid

--- liff-app-joystick/index.js
+++ liff-app-joystick/index.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID = '9645527a-5026-4733-9b63-1a9d7b0dad4c'; // LED, Button
+const USER_SERVICE_UUID = '7a83fff7-284e-49f2-bd4e-1c4bc396412c'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
